533f17f7719cf65ef2881d7cd2d54151cc0e314c
mehrad rafii <mehrad.rafii@yahoo.fr>
2018-02-24 15:13:01 +0100

    ajout de equals_aux et equals, non testés

--- files/projet.ml
+++ files/projet.ml
@@ -1,4 +1,5 @@
 (*Section 1*)
+#use "src/skeleton.ml";;
 
 let associate v1 v2 = 
    begin
@@ -34,9 +35,41 @@ let insert g o a la =
 
 (*Section 2*)
 
-let equals_aux g1 v1 g2 v2 = (true,[]);;
+let unmarked l = List.fold_right (fun s non_marques -> begin 
+														if Mark.get s == 0 then 
+															non_marques@[s]
+														else
+															non_marques
+													   end) l [];;
 
-let equals g1 v1 g2 v2 = (true,[]);;
+
+let rec equals_aux g1 v1 g2 v2 = 
+	let s1 = unmarked (ordered_succ g1 v1) and s2 = unmarked (ordered_succ g2 v2) in
+		if s1 == [] && s2 == [] then
+			(true,[])
+		else
+			if s1 == [] || s2 == [] then
+				(false,[])
+			else
+				let h1 = List.hd s1 and h2 = List.hd s2 in
+					begin
+						associate h1 h2;
+						let (bh,lh) = equals_aux g1 h1 g2 h2 and
+						(bq,lq) = equals_aux g1 v1 g2 v2 in
+							separate h1 h2;
+							let lf = lh@lq in
+								(bh && bq,[h1]@[h2]@lf);
+					end;;
+
+let equals g1 v1 g2 v2 = 
+	begin
+		associate v1 v2;
+		let (b,lr) = equals_aux g1 v1 g2 v2 in
+			let l = [v1]@[v2]@lr in
+				separate v1 v2;
+				(b,l);
+	end;;
+		
 
 
 (*Section 3*)
